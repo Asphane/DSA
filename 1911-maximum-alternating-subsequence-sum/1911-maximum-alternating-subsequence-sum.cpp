b5f4e1d8c99828a0d66d0396985f32d864bf2af6
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2025-12-29 23:10:01 +0530

    Time: 250 ms (46.92%), Space: 157.2 MB (38.57%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int n;
-    long long dp[100001][2];
+    // long long dp[100001][2];
 
-    long long solve(vector<int>& nums, int idx, int flag){
-        if(idx==n) return 0;
+    // long long solve(vector<int>& nums, int idx, int flag){
+    //     if(idx==n) return 0;
 
-        if(dp[idx][flag]!=-1)
-            return dp[idx][flag];
+    //     if(dp[idx][flag]!=-1)
+    //         return dp[idx][flag];
 
-        long long val=nums[idx];
+    //     long long val=nums[idx];
 
-        long long skip=solve(nums, idx+1, flag);
+    //     long long skip=solve(nums, idx+1, flag);
 
-        if(!flag)
-            val=-val;
+    //     if(!flag)
+    //         val=-val;
 
-        long long take=solve(nums, idx+1, !flag)+val;
+    //     long long take=solve(nums, idx+1, !flag)+val;
 
-        return dp[idx][flag]=max(take, skip);
-    }
+    //     return dp[idx][flag]=max(take, skip);
+    // }
 
     long long maxAlternatingSum(vector<int>& nums) {
         n=nums.size();
-        memset(dp, -1, sizeof(dp)); 
-        bool flag=true;
+        vector<vector<long long>> dp(n+1, vector<long long>(2, 0));
 
-        return solve(nums, 0, flag);
+        for(int i=1; i<n+1; i++){
+            dp[i][0]=max(dp[i-1][1]-nums[i-1], dp[i-1][0]);
+            dp[i][1]=max(dp[i-1][0]+nums[i-1], dp[i-1][1]);
+        }
+        return max(dp[n][0], dp[n][1]);
     }
 };
