af2faf54cd95aecc07ee1fbc7d827efa52e3e83a
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-01-01 10:36:01 +0530

    Time: 0 ms (100%), Space: 14 MB (87.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
     int n;
-    int dp[2501][2501];
-    int solve(vector<int> &nums, int idx, int prev){
-        if(idx>=n) return 0;
+    // int dp[2501][2501];
+    // int solve(vector<int> &nums, int idx, int prev){
+    //     if(idx>=n) return 0;
 
-        if(prev!=-1 && dp[idx][prev]!=-1) 
-            return dp[idx][prev];
+    //     if(prev!=-1 && dp[idx][prev]!=-1) 
+    //         return dp[idx][prev];
 
-        int take=0;
-        if(prev==-1 || nums[prev]<nums[idx])
-            take=1+solve(nums, idx+1, idx);
+    //     int take=0;
+    //     if(prev==-1 || nums[prev]<nums[idx])
+    //         take=1+solve(nums, idx+1, idx);
 
-        int leave=solve(nums, idx+1, prev);
+    //     int leave=solve(nums, idx+1, prev);
 
-        if(prev!=-1)
-            dp[idx][prev]=max(take, leave);
+    //     if(prev!=-1)
+    //         dp[idx][prev]=max(take, leave);
 
-        return max(take, leave);
-    }
+    //     return max(take, leave);
+    // }
 
     int lengthOfLIS(vector<int>& nums) {
         n=nums.size();
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
 
-        return solve(nums, 0, -1);
+        // return solve(nums, 0, -1);
+        vector<int> sorted;
+
+        for(int i=0; i<n; i++){
+            auto it=lower_bound(sorted.begin(), sorted.end(), nums[i]);
+
+            if(it==sorted.end())   
+                sorted.push_back(nums[i]);
+
+            else
+                *it=nums[i];
+        }
+        return sorted.size();
     }
 };
