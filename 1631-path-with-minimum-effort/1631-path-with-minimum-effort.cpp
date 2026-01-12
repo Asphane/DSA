227b941960b90eaa69881ba4964626e1a5f71194
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-01-12 19:44:47 +0530

    Time: 63 ms (42.93%), Space: 24.5 MB (56.4%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
-    int m, n;
     typedef pair<int, pair<int, int>> P;
-    vector<vector<int>> directions {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
     int minimumEffortPath(vector<vector<int>>& heights) {
-        m=heights.size();
-        n=heights[0].size();
+        int m=heights.size();
+        int n=heights[0].size();
+
+        vector<vector<int>> directions {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        priority_queue<P, vector<P>, greater<P>> pq;
+        vector<vector<int>> grid(m, vector<int> (n, INT_MAX));
+
+        pq.push({0, {0, 0}});
+        grid[0][0]=0;
 
         auto isSafe=[&](int x, int y){
             return x>=0 && y>=0 && x<m && y<n;
         };
 
-        priority_queue<P, vector<P>, greater<P>> pq;
-        vector<vector<int>> res(m, vector<int> (n, INT_MAX));
-        pq.push({0, {0, 0}});
-        res[0][0]=0;
-
         while(!pq.empty()){
-            int diff=pq.top().first;
-            auto node=pq.top().second;
+            auto curr=pq.top();
             pq.pop();
+
+            int diff=curr.first;
+            auto node=curr.second;
 
             int x=node.first;
             int y=node.second;
 
             for(auto &dir: directions){
-                int new_i=x+dir[0];
-                int new_j=y+dir[1];
+                int new_x=x+dir[0];
+                int new_y=y+dir[1];
 
-                if(isSafe(new_i, new_j)){
-                    int newDiff=max(diff, abs(heights[x][y]-heights[new_i][new_j]));
-
-                    if(newDiff < res[new_i][new_j]){
-                        res[new_i][new_j]=newDiff;
-                        pq.push({res[new_i][new_j], {new_i, new_j}});
+                if(isSafe(new_x, new_y)){
+                    int newDiff=max(diff, abs(heights[new_x][new_y]-heights[x][y]));
+                    if(newDiff<grid[new_x][new_y]){
+                        grid[new_x][new_y]=newDiff;
+                        pq.push({newDiff, {new_x, new_y}});
                     }
                 }
             }
         }
-        return res[m-1][n-1];
+        return grid[m-1][n-1];
     }
 };
