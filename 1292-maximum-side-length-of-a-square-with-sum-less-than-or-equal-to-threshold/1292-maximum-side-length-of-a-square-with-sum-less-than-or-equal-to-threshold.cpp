792f6674c2c6f4c459d5273d2cb7b49cc73459b5
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-01-22 19:56:31 +0530

    Time: 202 ms (8.32%), Space: 32.9 MB (37.57%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -5,15 +5,16 @@ public:
         int c=mat[0].size();
         int ans=0;
 
-        vector<vector<int>> preSum(r, vector<int>(c, 0));
+        vector<vector<int>> preSum(r, vector<int> (c, 0));
+
         preSum[0][0]=mat[0][0];
 
-        for(int i=1; i<c; i++){
-            preSum[0][i]=mat[0][i]+preSum[0][i-1];
+        for(int j=1; j<c; j++){
+            preSum[0][j]=preSum[0][j-1]+mat[0][j];
         }
 
         for(int i=1; i<r; i++){
-            preSum[i][0]=mat[i][0]+preSum[i-1][0];
+            preSum[i][0]=preSum[i-1][0]+mat[i][0];
         }
 
         for(int i=1; i<r; i++){
@@ -22,24 +23,21 @@ public:
             }
         }
 
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                for(int k=0; k<min(r-i, c-j); k++){
-                    int k_x=i+k;
-                    int k_y=j+k;
+        for(int side=0; side<min(r, c); side++){
+            for(int i=0; i+side<r; i++){
+               for(int j=0; j+side<c; j++){
+                    int new_i=i+side;
+                    int new_j=j+side;
 
-                    int sum=preSum[k_x][k_y]-(i>0 ? preSum[i-1][k_y]:0) 
-                            - (j>0 ? preSum[k_x][j-1]:0)
-                            + (i>0 && j>0 ? preSum[i-1][j-1]:0);
+                    int sum=preSum[new_i][new_j]
+                            -(i>0 ? preSum[i-1][new_j]:0)
+                            -(j>0 ? preSum[new_i][j-1]:0)
+                            +(i>0 && j>0 ? preSum[i-1][j-1]:0);
 
                     if(sum<=threshold){
-                        ans=max(ans, k+1);
+                        ans=max(ans, side+1);
                     }
-
-                    else{
-                        break;
-                    }
-                }
+                }    
             }
         }
         return ans;
