950862a0f1a7c4f1e63dd3dc7bdf8d454dccc954
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-01-31 19:44:27 +0530

    Time: 0 ms (100%), Space: 15.7 MB (70.48%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,36 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> mp;
-        queue<pair<TreeNode*, pair<int,int>>> q;
-        vector<vector<int>> res;
+    map<int, map<int, vector<int>>> mp;
+    queue<pair<TreeNode*, pair<int, int>>> q;
+    vector<vector<int>> res;
 
-        if (!root) return res;
+    void solve(TreeNode* root){
+        if(!root)
+            return;
 
-        q.push({root, {0, 0}}); // node, {column, row}
+        q.push({root, {0, 0}});
 
-        while (!q.empty()) {
-            auto front = q.front();
+        while(!q.empty()){
+            auto node = q.front();
             q.pop();
 
-            TreeNode* node = front.first;
-            int col = front.second.first;
-            int row = front.second.second;
+            TreeNode* curr = node.first;
+            auto dist = node.second;
 
-            mp[col][row].insert(node->val);
+            int col = dist.first;
+            int row = dist.second;
 
-            if (node->left)
-                q.push({node->left, {col - 1, row + 1}});
-            if (node->right)
-                q.push({node->right, {col + 1, row + 1}});
+            mp[col][row].push_back(curr->val);
+
+            if(curr->left)
+                q.push({curr->left, {col-1, row+1}});
+
+            if(curr->right)
+                q.push({curr->right, {col+1, row+1}});
+        }
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        if(!root)
+            return res;
+
+        solve(root);
+
+        for(auto &i: mp){
+            for(auto &j: i.second){
+                sort(j.second.begin(), j.second.end());
+            }
         }
 
-        for (auto &c : mp) {
-            vector<int> column;
-            for (auto &r : c.second) {
-                column.insert(column.end(), r.second.begin(), r.second.end());
+        for(auto i: mp){
+            vector<int> temp_col;
+            for(auto j: i.second){
+                for(auto k: j.second){
+                    temp_col.push_back(k);
+                }
             }
-            res.push_back(column);
+            res.push_back(temp_col);
         }
 
         return res;
