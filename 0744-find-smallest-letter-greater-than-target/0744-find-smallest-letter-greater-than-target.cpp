b46741bdeb8169e8edfb759bda6f2e99546b0e2a
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-01-31 15:14:37 +0530

    Time: 0 ms (100%), Space: 19.9 MB (24.67%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -6,9 +6,10 @@ public:
         int res=letters[0];
 
         for(int i=0; i<n; i++){
-            if(letters[i] > target && letters[i]-target < ans){
+            if(letters[i] > target){
                 ans=letters[i]-target;
                 res=letters[i];
+                break;
             }
         }
         return res;
