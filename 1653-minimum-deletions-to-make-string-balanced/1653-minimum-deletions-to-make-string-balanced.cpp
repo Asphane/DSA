b828ac2f45b5ea3b0a54c16495b8691199755df8
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-02-07 18:27:34 +0530

    Time: 19 ms (73.82%), Space: 26.4 MB (40.38%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        stack<char> st;
+        int cnt = 0;
+
+        for(char ch: s){
+            if(!st.empty() && st.top() == 'b' && ch == 'a'){
+                st.pop();
+                cnt++;
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        return cnt;
+    }
+};
