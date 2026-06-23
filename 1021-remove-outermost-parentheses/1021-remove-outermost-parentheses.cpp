03657946368094b37fc773f47ae597a190f7c9db
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-06-23 19:01:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.57%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n=s.size();
+        int cnt=0;
+        string res="";
+
+        for(char ch: s){
+            if(ch=='('){
+                if(cnt>0){
+                    res+=ch;
+                }
+                cnt++;
+            }
+
+            else{
+                cnt--;
+                if(cnt>0){
+                    res+=ch;
+                }
+            }
+        }
+        return res;
+    }
+};
