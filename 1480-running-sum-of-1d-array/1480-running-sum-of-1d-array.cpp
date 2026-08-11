148b7418395e064c27a8d22c18166eae717d03d9
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-08-11 10:39:53 +0530

    Time: 0 ms (100%), Space: 12.6 MB (53.6%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int sum=0;
+
+        for(int val: nums){
+            sum+=val;
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
