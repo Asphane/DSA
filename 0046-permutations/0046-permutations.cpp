ffaa2bc9562a64d7e58d1a998a1a94dca5beaf4a
Bisakh Patra <130459498+Asphane@users.noreply.github.com>
2026-08-22 11:00:17 +0530

    Time: 2 ms (36.05%), Space: 10.6 MB (51.81%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void permut(vector<int>& nums, vector<int>& path, vector<bool>& used){
+        if(path.size() == nums.size()){
+            ans.push_back(path);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(used[i]) continue;
+
+            path.push_back(nums[i]);
+            used[i]=true;
+
+            permut(nums, path, used);
+
+            path.pop_back();
+            used[i]=false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+
+        permut(nums, path, used);
+        return ans;
+    }
+};
